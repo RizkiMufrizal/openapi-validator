95839348a2cbe8993ede158421234f657b9ccc7b
RizkiMufrizal <mufrizalrizki@gmail.com>
2023-12-18 17:28:54 +0700

    add constant

---
+++ src/main/java/com/axway/library/common/Constant.java
@@ -0,0 +1,7 @@
+package com.axway.library.common;
+
+public class Constant {
+    public static final String apiManagerURL = "https://localhost:8075";
+    public static final String apiManagerVersion = "v1.4";
+    public static final String basePortalUrl = "/api/portal/";
+}
