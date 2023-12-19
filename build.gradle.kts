9ed8457c5cb3a0a0cce345eb5416892556955a90
RizkiMufrizal <mufrizalrizki@gmail.com>
2023-12-19 23:11:40 +0700

    add maven dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,15 +15,27 @@ configurations {
     }
 }
 
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.compileClasspath.map { config -> config.map { if (it.isDirectory) it else zipTree(it) } })
+}
 repositories {
     mavenCentral()
 }
 
+object DependencyVersions {
+    const val JACKSON_CORE = "2.16.0"
+    const val LOMBOK = "1.18.30"
+    const val HTTPCLIENT5 = "5.3"
+    const val SWAGGER_REQUEST_VALIDATOR_CORE = "2.39.0"
+    const val APIGW_DEPENDENCY = "7.7.0.20230830"
+}
+
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-core:2.16.0")
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.3")
-    implementation("com.atlassian.oai:swagger-request-validator-core:2.39.0")
-    implementation(fileTree(mapOf("dir" to "libs-7.7.0.20230830", "include" to listOf("*.jar"))))
+    implementation("com.fasterxml.jackson.core:jackson-core:${DependencyVersions.JACKSON_CORE}")
+    compileOnly("org.projectlombok:lombok:${DependencyVersions.LOMBOK}")
+    annotationProcessor("org.projectlombok:lombok:${DependencyVersions.LOMBOK}")
+    implementation("org.apache.httpcomponents.client5:httpclient5:${DependencyVersions.HTTPCLIENT5}")
+    implementation("com.atlassian.oai:swagger-request-validator-core:${DependencyVersions.SWAGGER_REQUEST_VALIDATOR_CORE}")
+    implementation(fileTree(mapOf("dir" to "apigw-dependencies/${DependencyVersions.APIGW_DEPENDENCY}", "include" to listOf("*.jar"))))
 }
