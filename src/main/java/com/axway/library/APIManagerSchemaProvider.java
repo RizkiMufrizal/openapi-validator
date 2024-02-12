34898b07e34df2bac966a11da7ad89b2fb819c4c
RizkiMufrizal <mufrizalrizki@gmail.com>
2024-02-12 10:07:14 +0700

    update log level

--- src/main/java/com/axway/library/APIManagerSchemaProvider.java
+++ src/main/java/com/axway/library/APIManagerSchemaProvider.java
@@ -23,13 +23,13 @@ public class APIManagerSchemaProvider {
     }
 
     public String getSchema(String apiId) throws Exception {
-        Utils.traceMessage("Getschema debug seb", TraceLevel.INFO);
+        Utils.traceMessage("Getschema debug seb", TraceLevel.DEBUG);
         if (!useOriginalAPISpec) {
 
-            Utils.traceMessage("call getFrontendAPISpec debug seb", TraceLevel.INFO);
+            Utils.traceMessage("call getFrontendAPISpec debug seb", TraceLevel.DEBUG);
             return getFrontendAPISpec(apiId);
         } else {
-            Utils.traceMessage("call getBackendAPIID debug seb", TraceLevel.INFO);
+            Utils.traceMessage("call getBackendAPIID debug seb", TraceLevel.DEBUG);
             String backendApiID = getBackendAPIID(apiId);
             Utils.traceMessage("Loading the original imported API specification from the API Manager using backend API-ID: " + backendApiID, TraceLevel.INFO);
             return getBackendAPISpec(backendApiID);
@@ -37,21 +37,21 @@ public class APIManagerSchemaProvider {
     }
 
     private String getFrontendAPISpec(String apiId) {
-        Utils.traceMessage("getFrontendAPISpec debug seb", TraceLevel.INFO);
-        Utils.traceMessage("getFrontendAPISpec debug seb inside try", TraceLevel.INFO);
+        Utils.traceMessage("getFrontendAPISpec debug seb", TraceLevel.DEBUG);
+        Utils.traceMessage("getFrontendAPISpec debug seb inside try", TraceLevel.DEBUG);
         var httpResponse = apiRepoRestClient.getSwagger("/discovery/swagger/api/id/" + apiId + "?swaggerVersion=2.0");
         int statusCode = httpResponse.getHttpCode();
 
-        Utils.traceMessage("getFrontendAPISpec debug seb response from 2.0 call" + httpResponse.getBody() + " code : " + statusCode, TraceLevel.INFO);
+        Utils.traceMessage("getFrontendAPISpec debug seb response from 2.0 call" + httpResponse.getBody() + " code : " + statusCode, TraceLevel.DEBUG);
         if (statusCode != 200) {
-            Utils.traceMessage("getFrontendAPISpec debug seb not 200", TraceLevel.INFO);
+            Utils.traceMessage("getFrontendAPISpec debug seb not 200", TraceLevel.ERROR);
             if (statusCode == 500) {
-                Utils.traceMessage("getFrontendAPISpec debug seb code = 500", TraceLevel.INFO);
+                Utils.traceMessage("getFrontendAPISpec debug seb code = 500", TraceLevel.ERROR);
                 if (httpResponse.getBody() != null) {
-                    Utils.traceMessage("getFrontendAPISpec debug seb response != null", TraceLevel.INFO);
+                    Utils.traceMessage("getFrontendAPISpec debug seb response != null", TraceLevel.ERROR);
                     if (httpResponse.getBody().contains("Failed to download API")) {
-                        Utils.traceMessage("getFrontendAPISpec debug seb response.contains", TraceLevel.INFO);
-                        Utils.traceMessage("No SwaggerVersion 2.0 API-Specification found, trying to download OpenAPI 3.0 specification.", TraceLevel.DEBUG);
+                        Utils.traceMessage("getFrontendAPISpec debug seb response.contains", TraceLevel.ERROR);
+                        Utils.traceMessage("No SwaggerVersion 2.0 API-Specification found, trying to download OpenAPI 3.0 specification.", TraceLevel.ERROR);
                         httpResponse = apiRepoRestClient.getSwagger("/discovery/swagger/api/id/" + apiId + "?swaggerVersion=3.0");
                         statusCode = httpResponse.getHttpCode();
                         if (statusCode == 200) {
@@ -68,7 +68,7 @@ public class APIManagerSchemaProvider {
 
     private String getBackendAPISpec(String backendApiId) throws Exception {
         var httpResponse = apiRepoRestClient.getSwagger("/apirepo/" + backendApiId + "/download?original=true");
-        Utils.traceMessage("getBackendAPISpec debug seb", TraceLevel.INFO);
+        Utils.traceMessage("getBackendAPISpec debug seb", TraceLevel.DEBUG);
         int statusCode = httpResponse.getHttpCode();
         if (statusCode != 200) {
             Utils.traceMessage("Error getting originally imported API-Specification from API-Manager. Received Status-Code: " + statusCode + ", Response: '" + httpResponse.getBody() + "'", TraceLevel.ERROR);
@@ -78,12 +78,12 @@ public class APIManagerSchemaProvider {
     }
 
     private String getBackendAPIID(String apiId) throws Exception {
-        Utils.traceMessage("getBackendAPIID debug seb", TraceLevel.INFO);
+        Utils.traceMessage("getBackendAPIID debug seb", TraceLevel.DEBUG);
         var httpResponse = apiRepoRestClient.getSwagger("/proxies/" + apiId);
         int statusCode = httpResponse.getHttpCode();
         if (statusCode != 200) {
             if (statusCode == 403 && httpResponse.getBody() != null && httpResponse.getBody().contains("Forbidden")) {
-                Utils.traceMessage("Please check the given API-ID: '" + apiId + "' is correct, as the API-Manager REST-API returns forbidden for an incorrect API-ID.", TraceLevel.INFO);
+                Utils.traceMessage("Please check the given API-ID: '" + apiId + "' is correct, as the API-Manager REST-API returns forbidden for an incorrect API-ID.", TraceLevel.ERROR);
             }
             Utils.traceMessage("Error loading Backend-API-ID from API Manager for API: " + apiId + ". Received Status-Code: " + statusCode + ", Response: '" + httpResponse.getBody() + "'", TraceLevel.ERROR);
             throw new IllegalArgumentException("Error loading Backend-API-ID from API Manager for API: " + apiId);
